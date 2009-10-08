0a8e6b949e4238e89bbcd511fdbb13435126ed9a
tiagochst <tiagochst@e7c76766-b3a3-11de-8af0-9d1a786950a5>
2009-10-08 02:17:33 +0000

    Inicio lab mc404

--- lab.asm
+++ lab.asm
@@ -20,41 +20,29 @@
 ; Format: .DEF rmp = R16
 ;
 ; ============================================
-;       S R A M   D E F I N I T I O N S
+;               M A C R O S
 ; ============================================
 ;
-.DSEG
-.ORG  0X0060
-; Format: Label: .BYTE N ; reserve N Bytes from Label:
-;
-; ============================================
-;   R E S E T   A N D   I N T   V E C T O R S
-; ============================================
-;
-.CSEG
-.ORG $0000
-	rjmp Main ; Reset vector
-	reti ; Int vector 1
-	reti ; Int vector 2
-	reti ; Int vector 3
-	reti ; Int vector 4
-	reti ; Int vector 5
-	reti ; Int vector 6
-	reti ; Int vector 7
-	reti ; Int vector 8
-	reti ; Int vector 9
-;
-; ============================================
-;     I N T E R R U P T   S E R V I C E S
-; ============================================
-;
-; [Add all interrupt service routines here]
+
+.macro ctabits1 ;find how many 1 there are in the vector 
+		; Input Parameters
+		; @0 e @1: dois registradores quaisquer (números para multiplcar)
+		; @2 um dos pares X,Y ou Z: endereço na RAM para colocar o produto no formato little endian
+
+
+.endmacro
+
+.macro clrbitvet ;find how many 1 there are in the vector 
+		; Input Parameters
+		; @0 e @1: vector adress,output
+.endmacro
+
 ;
 ; ============================================
 ;     M A I N    P R O G R A M    I N I T
 ; ============================================
 ;
-Main:
+Init:
 ; Init stack
 	ldi rmp, LOW(RAMEND) ; Init LSB stack
 	out SPL,rmp
@@ -64,12 +52,13 @@ Main:
 ; Init RAM
 	ldi yh, high(SRAM_START)	
 	ldi yl, low(SRAM_START)
-
+ret
 ;
 ; ============================================
 ;         P R O G R A M    L O O P
 ; ============================================
 ;
+
 Loop:
 	sleep ; go to sleep
 	nop ; dummy for wake up
