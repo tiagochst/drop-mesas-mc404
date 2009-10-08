97318f84052b6fa00e14282503a7ffeed76cb894
tiagochst <tiagochst@e7c76766-b3a3-11de-8af0-9d1a786950a5>
2009-10-08 01:39:18 +0000

    Inicio lab mc404

---
+++ lab.asm
@@ -0,0 +1,9 @@
+;lab.asm
+		;Nome:Alexandre Nobuo Kunieda	Ra:080523   
+		;Nome:Tiago Cheadraoui Silva 	Ra:082941
+		; mc 404 outubro de 2009
+;**********************************************************************************************************************************************
+.nolist
+.include "m88def.inc"
+.list
+
