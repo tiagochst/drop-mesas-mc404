33d508e673595a6e9dfef399ded50ef75a898100
tiagochst <tiagochst@e7c76766-b3a3-11de-8af0-9d1a786950a5>
2009-11-15 05:06:26 +0000

    POkes,explico no email.Altere o PINB (port B input pins) e vai ocorrer a parada do relogio. Usa o PCINT0 como interrupção posiçao 3 de memoria.

--- trunk/lab2.asm
+++ trunk/lab2.asm
@@ -59,11 +59,12 @@
 
 rjmp	RESET		;reset handle
 
-.org	0x01
-	rjmp    stop_count	;vetor de interrupção INT0 em 0x01
 .org	0x10
 	rjmp count1sec		; go to timer0 overflow counter interrupt routine
 
+.org 0x003
+	rjmp    stop_count	;vetor de interrupção INT0 em 0x01
+.org	0x100
 RESET:
     ldi r, low(RAMEND)
 	out	SPL,r		;initialize Stack Pointer to last RAM address
@@ -73,26 +74,24 @@ RESET:
 	rcall clockinit
 
 	;não tô entendendo nada
-	cbi DDRD,pd2		; configura p/ entrada no pino 4 (int0)
-	sbi DDRD,pd0		; saída: vamos ligar um led no pino 2 da CPU (pd0)
-	sbi PORTD,pd2		; ativa resistor de pull up; deveria colocar o bit PD2 de PIND em 1
 	ldi r,0
 	sts EICRA, r		; queremos interromper no nivel baixo do sinal em pd2 (p. 84 datasheet)
-	sbi eimsk,int0		; habilita interrupção INT0
 	ldi r,5				; sleep power down(SM1=1) & sleep enable SE=1(p 37 datasheet)
 	out SMCR,r			; do it 
-	;END não tô entendendo nada
 
 	ldi r,1			; era out TIMSK0,r no Atmega88 este registrador está fora do espaço de E/S!
+	sts PCICR, r               ; Ativa interrup. da porta B
+	sts PCMSK0,r               ; Bit 0 da porta B causa interrup.
 	sts TIMSK0,r	; enable timer0 overflow interrupt (p.102 datasheet)
 	ldi r,1			; set prescalong: 1= no prescaling 5=  CK/1024 pre-scaling (p 102-103 datasheet)
 	out TCCR0B,r	; also starts timer0 counting
-	out SMCR,r		; SMCR=1 selects idle mode sleep and enables sleep (p 37-38 datasheet)
 	sei		; Global Interrupt enable
 
+;	ldi r,1			; set prescalong: 1= no prescaling 5=  CK/1024 pre-scaling (p 102-103 datasheet)
+;	out TCCR0B,r	; also starts timer0 counting
+
+
 loop:
-	ldi r,1			; set prescalong: 1= no prescaling 5=  CK/1024 pre-scaling (p 102-103 datasheet)
-	out TCCR0B,r	; also starts timer0 counting
 
 	sleep				; "dorme" no modo power down: só acorda via interrupção externa
 	rjmp loop			; volta a "dormir" após serviço da interrupção
@@ -174,3 +173,8 @@ clockinit:
 	st Y+, r
 	st Y+, r
 ret
+
+
+
+
+teste: rjmp teste
