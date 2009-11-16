bf686793fd30792dd3d259e4caeef299139f9ee9
alexandre.kunieda <alexandre.kunieda@e7c76766-b3a3-11de-8af0-9d1a786950a5>
2009-11-16 07:32:31 +0000

    Hapsim funcionando razoavelmente. Está muito lento para imprimir o tempo no LCD, tem que ver se é assim mesmo, ou como arruma.

--- trunk/lab2.asm
+++ trunk/lab2.asm
@@ -57,13 +57,20 @@
 .def	r	=r16
 .def	tmp	=r17
 
+.equ LCDDATA=PORTD
+.equ LCDCTL=PORTC
+.EQU ENABLE=0
+.EQU RS=1
+.EQU RW=2
+.def    lcdinput    =   r19
+
 rjmp	RESET		;reset handle
 
 .org	0x10
 	rjmp count1sec		; go to timer0 overflow counter interrupt routine
 
 .org 0x003
-	rjmp    stop_count	;vetor de interrupção INT0 em 0x01
+	rjmp    toggle_clock	;vetor de interrupção INT0 em 0x01
 .org	0x100
 RESET:
     ldi r, low(RAMEND)
@@ -73,7 +80,6 @@ RESET:
 
 	rcall clockinit
 
-	;não tô entendendo nada
 	ldi r,0
 	sts EICRA, r		; queremos interromper no nivel baixo do sinal em pd2 (p. 84 datasheet)
 	ldi r,5				; sleep power down(SM1=1) & sleep enable SE=1(p 37 datasheet)
@@ -87,23 +93,32 @@ RESET:
 	out TCCR0B,r	; also starts timer0 counting
 	sei		; Global Interrupt enable
 
+	rcall lcdinit
+	rjmp LOOP
 
-loop:
-
+LOOP:
 	sleep				; "dorme" no modo power down: só acorda via interrupção externa
-	rjmp loop			; volta a "dormir" após serviço da interrupção
+	rjmp LOOP			; volta a "dormir" após serviço da interrupção
 
 
 ;**************************
 count1sec:
 	rcall clock
+
+	ldi lcdinput,2  ; init the LCD. 8 bit mode, 2*16
+	ldi zh, high(SRAM_START)
+	ldi zl, low(SRAM_START)
+	rcall lcd_cmd
+	rcall lcd_busy
+    rcall writemsg		; display it
 reti
 
-stop_count:				; rotina de interrupcao INT0
+toggle_clock:				; rotina de interrupcao INT0
 	in r, TCCR0B
-	com r
-	out TCCR0B, r		; stop timer0 counter: no more interrupts
-reti					; retorna com interrupções habilitadas
+	ldi tmp, 1
+	eor r, tmp
+	out TCCR0B, r		; toggle timer0 counter
+reti					; retorna com interrupções habilitadas/desabilitadas (inverso do que entrou)
 
 clock:
 	ldi yh, high(SRAM_START+6) ;seta Y para o final do cronometro
@@ -173,6 +188,76 @@ clockinit:
 ret
 
 
+;*************************************************************************************
+lcd_busy:
+; test the busy state
+sbi portc,RW        ; RW high to read
+cbi portc,RS        ; RS low to read
 
+ldi r16,0			; make port input
+out ddrd,r16
+out portd,r16
 
-teste: rjmp teste
+lcd_busy_loop:
+sbi portc,ENABLE    ; begin read sequence
+in r16,pind         ; read it
+cbi portc,ENABLE    ; set enable back to low
+;cbi portc,RW    ; clear the RW back to write mode
+sbrc r16,7          ; test bit 7, skip if clear
+rjmp lcd_busy_loop       ; jump if set
+
+ldi r16,0xff        ; make port output
+out ddrd,r16
+ret
+
+;****************************************************************************************
+lcd_cmd:
+
+; lcd_cmd writes the LCD command in r19 to the LCD
+cbi portc,RS    ; RS low for command mode
+cbi portc,RW    ; RW low to write
+sbi portc,ENABLE    ; Enable HIGH
+out portd,lcdinput  ; output
+cbi portc,ENABLE    ; Enable LOW to execute
+
+ret
+;*****************************************************************************************************
+lcd_write:
+
+; lcd_write writes the value in r19 to the LCD
+sbi portc,RS    ; RS high
+cbi portc,RW    ; RW low to write
+sbi portc,ENABLE    ; Enable HIGH
+out portd,lcdinput  ; output
+cbi portc,ENABLE    ; Enable LOW to execute
+
+ret
+;*****************************************************************
+writemsg:
+    ld lcdinput,z+      ; load r0 with the character to display          ; increment the string counter
+    cpi lcdinput, 0xFF
+	breq writedone
+    rcall lcd_write
+    rcall lcd_busy
+    rjmp writemsg
+writedone:
+ 	ret
+;*****************************************************************************************************
+lcdinit: 			;initialize LCD
+	ldi r16,0xff
+	out ddrd,r16 	;portb is the LCD data port, 8 bit mode set for output
+	out ddrc,r16	;portc is the LCD control pins set for output
+	ldi lcdinput,56  ; init the LCD. 8 bit mode, 2*16
+	rcall lcd_cmd    ; execute the command
+	rcall lcd_busy   ; test busy
+	ldi lcdinput,1		; clear screen
+	rcall lcd_cmd
+	rcall lcd_busy
+	;ldi lcdinput,15 	; show cursor and blink it
+	;rcall lcd_cmd
+	;rcall lcd_busy
+	ldi lcdinput,2      ; cursor home command
+    rcall lcd_cmd        ; execute command
+    rcall lcd_busy
+	ret
+;*****************************************************************************************************
