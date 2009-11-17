fe2a518e386ad8891ed1d72f1c7888c37f2f3d6c
tiagochst <tiagochst@e7c76766-b3a3-11de-8af0-9d1a786950a5>
2009-11-17 19:16:31 +0000

    Botão do Hapsim funcionando um pouco melhor.

--- lab2.asm
+++ lab2.asm
@@ -18,6 +18,13 @@
 ;  all used registers without specific names]
 .DEF r = r16
 .DEF tmp = r17
+.DEF lcdinput = r19
+
+.EQU LCDDATA = PORTD
+.EQU LCDCTL = PORTC
+.EQU ENABLE = 0
+.EQU RS = 1
+.EQU RW = 2
 
 
 rjmp RESET
@@ -52,9 +59,11 @@ RESET:
 	sts PCMSK0,r  ;activate PCINT0 interruption
 
 	sts TIMSK0,r  ;enable timer0 overflow interrupt
-	ldi r,1  ;set no prescaling
+	ldi r,0  ;set no prescaling
 	out TCCR0B,r  ;also starts timer0 counting
 
+	rcall lcdinit
+	rcall lcd_writetime
 	sei  ;global interrupt enable
 
 	rjmp MAINLOOP
@@ -67,22 +76,26 @@ RESET:
 count1sec:
 	adiw X, 1
 
-	ldi r, 0x0F
-	cpi xl, 0x42
+	;ldi r, 0x0F
+	;cpi xl, 0x42
+	ldi r, 0x0
+	cpi xl, 0x1
 	cpc xh, r  ;if the number of interruptions achieved 0xF42, 1 sec has gone
 	brne count1sec_exit
 
 	clr xh  ;restart counter of number of interruptions
 	clr xl
 	rcall clock
+	rcall lcd_writetime
 
 	count1sec_exit:
 reti
 
 toggle_clock:  ;INT0 interruption routine
-	in r, TCCR0B
-	ldi tmp, 1
-	eor r, tmp
+	in r, PINB
+	;in r, TCCR0B
+	;ldi tmp, 1
+	;eor r, tmp
 	out TCCR0B, r  ;toggle INT0 interruption
 reti  ;INT0 interruption will be disabled/abled (the opposite it started in this routine)
 
@@ -154,4 +167,83 @@ clockinit:
 	st Y+, r
 	st Y+, r
 	st Y+, r
+ret
+
+
+;
+; HAPSIM functions
+lcd_busy:
+	; test the busy state
+	sbi portc,RW        ; RW high to read
+	cbi portc,RS        ; RS low to read
+
+	ldi r16,0			; make port input
+	out ddrd,r16
+	out portd,r16
+
+	lcd_busy_loop:
+		sbi portc,ENABLE    ; begin read sequence
+		in r16,pind         ; read it
+		cbi portc,ENABLE    ; set enable back to low
+		;cbi portc,RW    ; clear the RW back to write mode
+		sbrc r16,7          ; test bit 7, skip if clear
+	rjmp lcd_busy_loop       ; jump if set
+
+	ldi r16,0xff        ; make port output
+	out ddrd,r16
+ret
+
+lcd_cmd:
+	; lcd_cmd writes the LCD command in r19 to the LCD
+	cbi portc,RS    ; RS low for command mode
+	cbi portc,RW    ; RW low to write
+	sbi portc,ENABLE    ; Enable HIGH
+	out portd,lcdinput  ; output
+	cbi portc,ENABLE    ; Enable LOW to execute
+ret
+
+lcd_write:
+	; lcd_write writes the value in r19 to the LCD
+	sbi portc,RS    ; RS high
+	cbi portc,RW    ; RW low to write
+	sbi portc,ENABLE    ; Enable HIGH
+	out portd,lcdinput  ; output
+	cbi portc,ENABLE    ; Enable LOW to execute
+ret
+
+writemsg:
+	ld lcdinput,z+      ; load r0 with the character to display          ; increment the string counter
+	cpi lcdinput, 0xFF
+	breq writedone
+	rcall lcd_write
+	rcall lcd_busy
+	rjmp writemsg
+writedone:
+ret
+
+lcdinit: 			;initialize LCD
+	ldi r16,0xff
+	out ddrd,r16 	;portb is the LCD data port, 8 bit mode set for output
+	out ddrc,r16	;portc is the LCD control pins set for output
+	ldi lcdinput,56  ; init the LCD. 8 bit mode, 2*16
+	rcall lcd_cmd    ; execute the command
+	rcall lcd_busy   ; test busy
+	ldi lcdinput,1		; clear screen
+	rcall lcd_cmd
+	rcall lcd_busy
+	;ldi lcdinput,15 	; show cursor and blink it
+	;rcall lcd_cmd
+	;rcall lcd_busy
+	ldi lcdinput,2      ; cursor home command
+	rcall lcd_cmd        ; execute command
+	rcall lcd_busy
+ret
+
+lcd_writetime:
+	ldi lcdinput,2  ; init the LCD. 8 bit mode, 2*16
+	ldi zh, high(SRAM_START)
+	ldi zl, low(SRAM_START)
+	rcall lcd_cmd
+	rcall lcd_busy
+	rcall writemsg  ; display it
 ret
